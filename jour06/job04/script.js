107889d7046c42a355a2235037828600628f08a4
Thibault <thibault.kine@laplateforme.io>
2022-03-21 13:02:39 +0100

    fini jour 6

---
+++ jour06/job04/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+var burger = document.getElementById("burger");
+var burgerImg = document.getElementById('burger-img');
+var nav = document.getElementById("burger-links");
+
+var isShown = false;
+console.log(burgerImg)
+
+burgerImg.addEventListener('click', () => {
+    isShown = !isShown;
+    console.log(isShown);
+
+    if(isShown) {
+        nav.style.display = 'flex';
+        nav.style.flexDirection = 'column';
+        nav.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+        nav.style.fontSize = '20em';
+        nav.style.width = '70%';
+        nav.style.padding = '10%';
+        nav.style.justifyContent = 'space-evenly';
+    }
+    else {
+        nav.style.display = 'none';
+    }
+})
+
+})
